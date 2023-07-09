1b5dfec3895311b9d984fbeb4216a28f90802ff7
Asher Etherington <asherce21@gmail.com>
2023-07-08 22:00:53 -0700

    finished with 2 projects added

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
@@ -20,7 +20,9 @@ projects = [
         'prod': 'https://flask-microblog-utz5.onrender.com/'
     },
 ]
-
+# create a dictionary to map slugs to projects so that we don't have to loop through all projects
+# whenever we want to get a slug
+slug_to_project = {project['slug']: project for project in projects}
 
 @app.route("/")
 def home():
@@ -35,3 +37,13 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/project/<string:slug>")
+def project(slug):
+    # if the slug doesn't exist return a 404 error
+    if slug not in slug_to_project:
+        abort(404)
+    # return a render template using the slug to get the needed html page
+    return render_template(f"project_{slug}.html", 
+                           project=slug_to_project[slug]
+                           )
